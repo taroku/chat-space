44f9aa2c978e56b7a9f694b34d91ec7f2ce5b8e1
tm <tm@tmnoMacBook-puro.local>
2019-02-10 21:37:03 +0900

    create user_controller and fix views and etc

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,7 @@ Rails.application.routes.draw do
   devise_for :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
   root 'messages#index'
+  resources :users, only: [:edit, :update]
   get 'messages' => 'messages#index' #サインイン後、トップ（チャット画面）
   post 'messages/new' => 'messages#new'
 end
