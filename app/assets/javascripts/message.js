e447ca5ec8f805e0a7739d9e63845058286f841c
tm <tm@tmnoMacBook-puro.local>
2019-03-03 01:11:59 +0900

    create ajax

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -1,7 +1,46 @@
 $(function(){
+  function buildHTML(message){
+    var html = `<div class="chat-main__body">
+                <div class="chat-main__body--messages-list">
+                <div class="chat-main__message clearfix">
+                <div class="chat-main__message-name">
+                ${message.name}
+                </div>
+                <div class="chat-main__message-time">
+                ${message.created_at}
+                </div>
+                <div class="chat-main__message-body">
+                <p class="lower-message__content">
+                ${message.content}
+                </p>
+                </div>
+                </div>
+                </div>
+                </div>`
+    return html;
+  }
   $('#new_message').on('submit', function(e){
-    var formData = new FormData(this);
     e.preventDefault();
-    console.log(this);
+    var formData = new FormData(this);
+    var url = $(this).attr('action')
+    $.ajax({
+      url: url,
+      type: 'POST',
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $('.messages').append(html)
+      $('.textbox').val('')
+      $("html,body").animate({scrollTop:$('#chat-main__footer').offset().top});
+    })
+    .fail(function(){
+      alert('error');
+    })
+    // e.preventDefault();
+    // console.log(this);
   })
 })
